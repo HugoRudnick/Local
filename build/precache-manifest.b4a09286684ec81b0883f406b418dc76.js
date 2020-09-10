5c4b6c9d31e04594a637978a1f37418a0ae647d0
HugoRudnick <hugoo_fr@hotmail.com>
2020-09-09 23:23:21 -0300

    Local

---
+++ build/precache-manifest.b4a09286684ec81b0883f406b418dc76.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7f83b9028b10d18f965e28aaefcaa00",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f39c48dc7e8356b513cc",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "474fa23de89fb9891b80",
+    "url": "/static/js/2.1b4eeeeb.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.1b4eeeeb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f39c48dc7e8356b513cc",
+    "url": "/static/js/main.3445e526.chunk.js"
+  },
+  {
+    "revision": "5387050906b8ed53cb42",
+    "url": "/static/js/runtime-main.ec2d9410.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
